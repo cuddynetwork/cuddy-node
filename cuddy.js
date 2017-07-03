8828459701f85dbe6a0089be9aa926f8204ac9b9
Jakub Wierzcholski <wierzcholski@protonmail.com>
2017-07-03 13:44:41 +0200

    Egzample structure of Contract, probably need a lot of fixes

--- cuddy.js
+++ cuddy.js
@@ -3,6 +3,22 @@
     0.0.0
 */
 
+/* Ladgers */
+
+var Contract = {
+    tx : "default",
+    submited: 0, //timestamp of submmision
+    expireDate: 0, //timestamp of expiration
+    fee: {
+        storage: 0,
+        proccessing: 0,
+        transfer: 0
+    },
+    nodes: {
+        //must be a vector of nodes
+    }
+    
+} 
 
 /* Events listening */
 
