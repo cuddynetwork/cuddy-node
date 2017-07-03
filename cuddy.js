c1245b1ebba04f28157eaaa7372ee5c811018b66
Jakub Wierzcholski <wierzcholski@protonmail.com>
2017-07-03 18:08:04 +0200

    Adding structures of ladger of Nodes Reputation, Nodes Connected

--- cuddy.js
+++ cuddy.js
@@ -5,6 +5,7 @@
 
 /* Ladgers */
 
+// For ladger of contracts
 var Contract = {
     tx : "default",
     submited: 0, //timestamp of submmision
@@ -22,11 +23,36 @@ var Contract = {
     reqLastMinute: 0, //amount of resource request (will be used to ajust amount of nodes to store file)
     dataChecksum: "default", // calculated from data
     userChecksum: "default" // calculated from dataChecksum and private key, can by verify by public key and dataCheckSum
+}; 
+// For ladger of reputation
+var Node = {
+    nodeId: "default", // uniq hash, don't know if needed
+    ip: "255.255.255",
+    port: 7733, // port to comunicate
+    startTime: 0, // Timestamp of node first notice in the network 
+    lastActive: 0, //Last activity of node in the network
+    checkPositive: 0, //Possitive check by another node
+    checkNoResponse: 0, //Without response / timeout
+    checkNegative: 0, // With incorect checksum
+    capacityAvaiable: 0, // In bytes 
+    capacityUsed: 0, // Used capacity, not know if needed, we can calculate it from ladgerOfContracts
     
-} 
+    
+};
+
+// My connections, to stop spaming each other with a lot of information, use maximum 5 nodes to push and get information, one communicate will be recived only 5 times in worstes scenario 
+
+// max length of vector about 5 nodes
+var Connection = {
+    NodeId: "default",
+    ip:"255.255.255" // more can get from NodeLadger, no need for save twice
+}
+
+
 
 /* Events listening */
 
 const DEFAULT_PORT_PUBLIC = 80;
 const DEFAULT_PORT_COMUNICATION = 7733;
+
 
