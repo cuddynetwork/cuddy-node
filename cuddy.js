2176639e49f7a16e2c3685c9ed796f7e337051a5
Jakub Wierzcholski <wierzcholski@protonmail.com>
2017-07-03 18:25:28 +0200

    New parameters for ladgers

--- cuddy.js
+++ cuddy.js
@@ -15,15 +15,20 @@ var Contract = {
         proccessing: 0,
         transfer: 0
     },
+    requestAll: 0, // how much requests has been made
+    usedTransfer: 0, // each node will update that after work with consensus
+    proccessUsage: 0, // some unit to control proccess power usage
+    lastStoragePayPeriode: 0, // timestamp of last pay time for storage
     nodes: {
         //must be a vector of nodes
     },
     complexity: 0, //calculated by node which recived contract,
     size: 0, //can be check by node to verify if not forgery
     reqLastMinute: 0, //amount of resource request (will be used to ajust amount of nodes to store file)
+    
     dataChecksum: "default", // calculated from data
     userChecksum: "default" // calculated from dataChecksum and private key, can by verify by public key and dataCheckSum
-}; 
+};
 // For ladger of reputation
 var Node = {
     nodeId: "default", // uniq hash, don't know if needed
@@ -37,7 +42,6 @@ var Node = {
     capacityAvaiable: 0, // In bytes 
     capacityUsed: 0, // Used capacity, not know if needed, we can calculate it from ladgerOfContracts
     
-    
 };
 
 // My connections, to stop spaming each other with a lot of information, use maximum 5 nodes to push and get information, one communicate will be recived only 5 times in worstes scenario 
@@ -45,7 +49,7 @@ var Node = {
 // max length of vector about 5 nodes
 var Connection = {
     NodeId: "default",
-    ip:"255.255.255" // more can get from NodeLadger, no need for save twice
+    ip: "255.255.255" // more can get from NodeLadger, no need for save twice
 }
 
 
@@ -54,5 +58,3 @@ var Connection = {
 
 const DEFAULT_PORT_PUBLIC = 80;
 const DEFAULT_PORT_COMUNICATION = 7733;
-
-
