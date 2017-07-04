10eeca1f01ddb210d88525777f14b2bbf4211d5d
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-04 23:04:52 +0200

    Negotiator

--- cuddy-events/Negotiator.js
+++ cuddy-events/Negotiator.js
@@ -1,1 +1,18 @@
+/*
+    Module for processing contract negotiation on your node
+    CuddyCore v. 0.0.0
+*/
 
+/// Function for handling contract negotiation on this node
+function processContractNegotiation() {
+
+  if (contract.fileSize <= MAX_ALLOWED_FILE_SIZE) {
+
+        if (contract.complexity <= MAX_ALLOWED_COMPLEXITY) {
+
+
+        }
+
+    }
+
+}
