f770ae6e15181cc85159f0bb4bd8ee88e16ed62c
Jakub Wierzcholski <wierzcholski@protonmail.com>
2017-07-04 19:43:43 +0200

    Add HandleOfSending function to Broadcast

---
+++ cuddy-events/HandelSend.js
@@ -0,0 +1,8 @@
+// Direct broadcast to one node
+function BroadcastToOne(data,typeOfCommunication,direction){
+    
+}
+// Broadcast to whole list of nodes from connected nodes
+function BroadcastToAll(data,typeOfCommunication){
+    
+}
