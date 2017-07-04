0771f09aa08cf98d5d90042d9af19b893d8a29d7
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-04 20:55:48 +0200

    Update BlockchainProcessor.js

--- cuddy-events/BlockchainProcessor.js
+++ cuddy-events/BlockchainProcessor.js
@@ -2,3 +2,17 @@
     Module for broadcasting and getting nodes and contract information from Blockchain
     CuddyCore v. 0.0.0
 */
+
+
+function saveContractInBlockchain() {
+   // Save contract and its details in blockchain
+
+
+
+}
+
+function saveContractResourceLocation() {
+   // Save next resource location and assign to contractId
+
+
+}
