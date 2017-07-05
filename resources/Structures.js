614b3fca235d17c580f5562431d8ec801bfd6dc4
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-05 21:26:09 +0200

    Create Structures.js

---
+++ resources/Structures.js
@@ -0,0 +1,64 @@
+/*
+    Data structures for Cuddy
+    CuddyCore v. 0.0.0
+*/
+
+/* Ledgers */
+
+// For ledger of contracts
+var Contract = {
+    tx : "default",
+    type: "static", // static or script
+    submited: 0, //timestamp of submmision
+    expireDate: 0, //timestamp of expiration
+    fee: {
+        storage: 0,
+        execution: 0,
+        transfer: 0
+    },
+    requestAll: 0, // how much requests has been made
+    usedTransfer: 0, // each node will update that after work with consensus
+    proccessUsage: 0, // some unit to control proccess power usage
+    lastStoragePayPeriode: 0, // timestamp of last pay time for storage
+    nodes: {
+        //must be a vector of nodes
+    },
+    complexity: 0, //calculated by node which recived contract,
+    size: 0, //can be check by node to verify if not forgery
+    reqLastMinute: 0, //amount of resource request (will be used to ajust amount of nodes to store file)
+
+    dataChecksum: "default", // calculated from data
+    userChecksum: "default" // calculated from dataChecksum and private key, can by verify by public key and dataCheckSum
+};
+
+// For ledger of reputation
+var Node = {
+    nodeId: "default", // unique hash
+    ip: "255.255.255",
+    port: 6689, // port to comunicate
+    startTime: 0, // Timestamp of node first notice in the network
+    lastActive: 0, //Last activity of node in the network
+    checkPositive: 0, //Possitive check by another node
+    checkNoResponse: 0, //Without response / timeout
+    checkNegative: 0, // With incorect checksum
+    capacityAvaiable: 0, // In bytes
+    capacityUsed: 0, // Used capacity, not know if needed, we can calculate it from ladgerOfContracts
+
+};
+
+// My connections, to stop spaming each other with a lot of information, use maximum 5 nodes to push and get information, one communicate will be recived only 5 times in worstes scenario
+
+// max length of vector about 5 nodes
+var Connection = {
+    NodeId: "default",
+    ip: "255.255.255" // more can get from NodeLadger, no need for save twice
+}
+
+var CloneRequest = {
+    contractID: "default"
+
+}
+
+
+exports.Contract = Contract;
+exports.Node = Node;
