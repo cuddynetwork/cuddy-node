180cac6feb5e1bef97cae0bea327a8773d45dd87
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-05 22:24:51 +0200

    Add Contracts ledger mechanisms

--- cuddy.js
+++ cuddy.js
@@ -5,12 +5,15 @@
 
 var WebSocketServer = require('websocket').server;
 var http = require('http');
-var Hashes = require('jshashes')
+var Hashes = require('jshashes');
 var crypto = require('crypto');
 var KBucket = require('k-bucket');
 var colors = require('colors/safe');
 var dateTime = require('node-datetime');
 var WebSocketClient = require('websocket').client;
+
+var ContractsLedgerProcessor = require('./cuddy-events/ContractsLedgerProcessor.js');
+var Structures = require('./resources/Structures.js');
 
 
 /* Constants */
@@ -54,9 +57,12 @@ if (firstrun) {
 
 }
 
-localNodeID = "ffcdd3449fe7c039ae93aac4831768ace43c6ffa243103d1b871f90add264b9121876e9576309183";
-localNodeIP = "89.231.22.170";
+localNodeID = "fdcdd3449fe7c039ae93aac4831768ace43c6ffa243103d1b871f90add264b9121876e9576309183";
+localNodeIP = "79.231.22.170";
 localNodePort = "6689";
+
+var Node = Structures.Node;
+var Contract = Structures.Contract;
 
 console.log(new Date(dt.now()) + " " + colors.green('Cuddy node started!'));
 
@@ -122,77 +128,36 @@ client.on('connect', function(connection) {
 
 });
 
-
-
-client.connect('ws://localhost:6689//', 'cuddy-protocol');
-
-
-/* Ledgers */
-
-// For ledger of contracts
-var Contract = {
-    tx : "default",
-    type: "static", // static or script
-    submited: 0, //timestamp of submmision
-    expireDate: 0, //timestamp of expiration
-    fee: {
-        storage: 0,
-        execution: 0,
-        transfer: 0
-    },
-    requestAll: 0, // how much requests has been made
-    usedTransfer: 0, // each node will update that after work with consensus
-    proccessUsage: 0, // some unit to control proccess power usage
-    lastStoragePayPeriode: 0, // timestamp of last pay time for storage
-    nodes: {
-        //must be a vector of nodes
-    },
-    complexity: 0, //calculated by node which recived contract,
-    size: 0, //can be check by node to verify if not forgery
-    reqLastMinute: 0, //amount of resource request (will be used to ajust amount of nodes to store file)
-
-    dataChecksum: "default", // calculated from data
-    userChecksum: "default" // calculated from dataChecksum and private key, can by verify by public key and dataCheckSum
-};
-
-// For ledger of reputation
-var Node = {
-    nodeId: "default", // unique hash
-    ip: "255.255.255",
-    port: 6689, // port to comunicate
-    startTime: 0, // Timestamp of node first notice in the network
-    lastActive: 0, //Last activity of node in the network
-    checkPositive: 0, //Possitive check by another node
-    checkNoResponse: 0, //Without response / timeout
-    checkNegative: 0, // With incorect checksum
-    capacityAvaiable: 0, // In bytes
-    capacityUsed: 0, // Used capacity, not know if needed, we can calculate it from ladgerOfContracts
-
-};
-
-// My connections, to stop spaming each other with a lot of information, use maximum 5 nodes to push and get information, one communicate will be recived only 5 times in worstes scenario
-
-// max length of vector about 5 nodes
-var Connection = {
-    NodeId: "default",
-    ip: "255.255.255" // more can get from NodeLadger, no need for save twice
-}
-
-var CloneRequest = {
-    contractID: "default"
-
-}
-
-
-//curent_node.ip = "345";
-
-
+client.connect('ws://cuddy.network:6689//', 'cuddy-protocol');
 
 /* Create WebSocket Listening Server */
 
 var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
+  // process HTTP request.
+  // Also this is used to initial serving websockets
+
+  //console.log(request);
+
+  if (request.url == "/cuddy_contracts_ledger") {
+
+      console.log(new Date(dt.now()) + " " + 'Received contracts ledger upload HTTP request from remote client');
+
+      response.writeHead(200, {"Content-Type": "text/json"});
+
+      current_contract_ledger_content = ContractsLedgerProcessor.getContractsLedgerContent();
+
+      response.write( JSON.stringify(current_contract_ledger_content));
+      response.end();
+  } else {
+
+    console.log(new Date(dt.now()) + " " + colors.yellow('Received unrecognized HTTP request from remote client'));
+    response.writeHead(200, {"Content-Type": "text/json"});
+    response.write('{"error":"UNRECOGNIZED_REQUEST"}');
+    response.end();
+
+  }
+
+
 });
 server.listen(DEFAULT_PORT_COMUNICATION, function() { });
 
@@ -351,7 +316,7 @@ wsServer.on('request', function(request) {
             console.log(new Date(dt.now()) + " " + 'Sending NODE_ANNOUCE to remote node');
             connection.sendUTF(JSON.stringify(NodeAnnouceResponse));
 
-        }  else if (json.name == "PING") {
+        }  else if (json.method == "PING") {
             /// handle Ping with Pong responde send
             console.log(new Date(dt.now()) + " " + 'Received PING message from remote client, so sending PONG :D');
 
@@ -366,12 +331,18 @@ wsServer.on('request', function(request) {
 
               connection.sendUTF(JSON.stringify(PongResponse));
         }
-         else if (json.name == "PONG") {
+         else if (json.method == "PONG") {
             /// handle Pong with to que get (PongQueue)
             connection.sendUTF('{  }');
 
-          }
+          } else if (json.method == "PUBLISH_CONTRACT") {
 
+                  /// publish negotiated contract to the network
+                  ContractsLedgerProcessor.pushContract(json.contract.tx, json.contract);
+
+                  connection.sendUTF('{"result":"SAVED"}');
+
+          }
     }
   });
 
