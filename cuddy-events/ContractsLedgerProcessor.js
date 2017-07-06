a8122b6b0ce4dbf967bbcad0bd4bf2cccc5bcc8e
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-06 12:02:03 +0200

    Update ContractsLedgerProcessor.js

--- cuddy-events/ContractsLedgerProcessor.js
+++ cuddy-events/ContractsLedgerProcessor.js
@@ -19,6 +19,17 @@ var db = new JsonDB("cuddy-contracts-ledger", true, true);
 
 module.exports = {
 
+addNewNodeToContract: function (contract_id, nodeID) {
+
+  nodes_count = (module.exports.countNodesInContract(contract_id)+1).toString();
+
+  db.push("/contracts/" + contract_id.toString() + "/nodes/" + nodes_count, nodeID);
+  db.save();
+  return true;
+
+},
+
+
 removeJunkContracts: function () {
 
 
@@ -29,21 +40,71 @@ checkForExpirations: function () {
 
 },
 
-pushContract: function (id, contract) {
+pushContract: function (contract_id, contract) {
 
-    db.push("/contracts/" + id.toString(),contract);
+    db.push("/contracts/" + contract_id.toString(), contract);
     db.save();
     return true;
 
 },
+
+
+getContract: function (contract_id) {
+
+    contract = db.getData("/contracts/" + contract_id.toString());
+
+    return contract;
+
+},
+
 
 downloadContractsLedger: function  (nodes_number) {
 
 
 },
 
+getContractNodesIds: function  (contract_id) {
 
+  contract_nodes_ids = db.getData("/contracts/" + contract_id.toString() + "/nodes");
+
+  return contract_nodes_ids;
+
+
+},
+
+countNodesInContract: function  (contract_id) {
+
+  return Object.keys(module.exports.getContractNodesIds(contract_id)).length;
+
+},
+
+/*
+    Returns nodeID string for provided node index
+*/
+getContractNodeIDByIndex: function  (contract_id, node_index) {
+
+  contract_node_id = db.getData("/contracts/" + contract_id.toString() + "/nodes/" + node_index.toString());
+  return contract_node_id.toString();
+
+},
+
+/*
+    Returns JSON containing full ledger of contracts
+*/
 getContractsLedgerContent: function () {
+
+  return db.getData("/contracts");
+
+},
+
+
+getContractNodeByIndex: function (contract_id, node_index) {
+
+  return db.getData("/contracts");
+
+},
+
+getContractNodeByID: function (contract_id, nodeID) {
 
   return db.getData("/contracts");
 
