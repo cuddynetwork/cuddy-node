f2cbee14aece74f6a8222289b2e911bf1e153900
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-09 13:50:48 +0200

    Update Collect.js

--- cuddy-events/Collect.js
+++ cuddy-events/Collect.js
@@ -4,8 +4,40 @@
 */
 
 
-CollectTokensBucket = []
+var Structures = require('../resources/Structures.js');
+var Constants = require('../resources/Constants.js');
 
-function generateCollectToken() {
 
-}
+/// Init JSON database ////
+
+var JsonDB = require('node-json-db');
+
+var db = new JsonDB(LEDGERS_DEFAULT_SAVE_LOCATION + "/.cuddy/ledgers/cuddy-local-collect-tokens-ledger", true, true);
+
+const LEDGERS_DEFAULT_SAVE_LOCATION = Constants.LEDGERS_DEFAULT_SAVE_LOCATION;
+
+
+module.exports = {
+
+  generateCollectToken: function() {
+
+    var collectToken = crypto.randomBytes(64).toString('hex');
+    return collectToken;
+
+  },
+
+  putCollectTokenAndDetails: function(collect_token, token_details) {
+
+      db.push("/collect_tokens/" + collect_token.toString(), token_details);
+      return true;
+
+  },
+
+  getCollectTokenDetails: function(collect_token_id) {
+
+    var collectTokenDetails = db.getData("/collect_tokens/" + collect_token_id.toString()).toString();
+    return collectTokenDetails;
+
+  }
+
+};
