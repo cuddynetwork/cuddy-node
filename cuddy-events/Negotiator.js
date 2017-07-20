28bd605e10a1ded4da5d91464006b8a4adbb61ed
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-20 17:24:52 +0200

    Fix code mistakes

--- cuddy-events/Negotiator.js
+++ cuddy-events/Negotiator.js
@@ -3,34 +3,36 @@
     CuddyCore v. 0.0.0
 */
 
+var LocalNode = require('./MyNode.js');
+
 /// Function for handling contract negotiation on this node
-function processContractNegotiation() {
+function processContractNegotiation(contractProposal) {
 
-  if (contract.fileSize <= MAX_ALLOWED_FILE_SIZE) {
+  if (contractProposal.fileSize <= MAX_ALLOWED_FILE_SIZE) {
 
-        if (contract.complexity <= MAX_ALLOWED_COMPLEXITY) {
-            
+        if (contractProposal.complexity <= MAX_ALLOWED_COMPLEXITY) {
+
             // CHECK IF NEEDED WITH THIS RATINGS
-            var VariableBaseOnContractNeed = 1
-            var NodesMinAmount = 5 * VariableBaseOnContractNeed
-            
-            
-            var ListOfNodes = getContractNodes(contract.tx);
-            
+            var VariableBaseOnContractNeed = 1;
+            var NodesMinAmount = 5 * VariableBaseOnContractNeed;
+
+
+            var ListOfNodes = LocalNode.getContractNodes(contractProposal.tx);
+
             var NodeTem,NodeRatting;
             var SumUpRating = 0;
             var i=0;
-            while(ListOfNodes[i]){
-                NodeTem = getNodeDetailsByID(ListOfNodes[i]);
+            for (nodeid in ListOfNodes){
+                NodeTem = LocalNode.getNodeDetailsByID(ListOfNodes[nodeid]);
                 if(NodeTem.checkPositive)!=0)
                     NodeRatting = NodeTem.checkPositive / (NodeTem.checkPositive + NodeTem.checkNoResponse)
                 else
                     NodeRatting = 0
-                
+
                 SumUpRating += NodeRatting
                 i++
             }
-            
+
             if(SumUpRating < NodesMinAmount){
                 // free space
                 var SumUpLadger = 0
@@ -40,21 +42,52 @@ function processContractNegotiation() {
                  return true
                 }
                 else{
-                    //CHECK IF competitive 
+                    //CHECK IF competitive
                     //Find smallest RateCompetitive for this node
-                    
-                    var RateCompetitive = 
-                        Math.min(competitive.storage * fee.storage,
-                        competitive.execution * fee.execution,
-                        competitive.transfer * fee.transfer);
+                    competitive = LocalNode.getLocalNodeCompetitive();
+
+                    var RateCompetitive = competitive.storage * contractProposal.fee.storage +
+                        competitive.execution * contractProposal.fee.execution +
+                        competitive.transfer * contractProposal.fee.transfer;
                     //contract.fee.storage
-                }    
+                }
             }
-            
-            
-            
+
+
+
 
         }
     }
 
 }
+
+/// TEST
+
+var Contract = {
+    tx : "default",
+    type: "static", // static or script
+    submited: 0, //timestamp of submmision
+    expireDate: 0, //timestamp of expiration
+    fee: {
+        storage: 0,
+        execution: 0,
+        transfer: 0
+    },
+    requestAll: 0, // how much requests has been made
+    usedTransfer: 0, // each node will update that after work with consensus
+    proccessUsage: 0, // some unit to control proccess power usage
+    lastStoragePayPeriode: 0, // timestamp of last pay time for storage
+    nodes: {
+        //must be a vector of nodes
+    },
+    complexity: 0, //calculated by node which recived contract,
+    size: 0, //can be check by node to verify if not forgery
+    reqLastMinute: 0, //amount of resource request (will be used to ajust amount of nodes to store file)
+
+    dataChecksum: "default", // calculated from data
+    userChecksum: "default", // calculated from dataChecksum and private key, can by verify by public key and dataCheckSum
+    sslEnabled: 0 // notify node if SSL (HTTPS) is enabled
+
+};
+
+processContractNegotiation(Contract);
